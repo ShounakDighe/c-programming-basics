85a25d836e20dbf2aec85bc605cdb504531a4b1f
Shounak Dighe <shounakdighe@gmail.com>
2023-09-27 21:03:06 +0530

    prime

--- prime.c
+++ prime.c
@@ -5,12 +5,12 @@ int main(){
 	int prime;
 	printf("Enter Your number : ");
 	scanf("%d",&prime);
-	for {
+	
 		if (prime%2==0) {
 		printf("It is a composite Number\n ");
 		}
 		else{
 		printf("It is a prime Number\n");
 		}	
-	}
+	
 }
